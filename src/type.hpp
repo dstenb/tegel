eb1af1f9d776928be5c5c01e14710be8d2feec4f
dstenb <dstenb@gmail.com>
2013-06-26 14:41:21 +0000

    Added InvalidTypeError class

--- src/type.hpp
+++ src/type.hpp
@@ -1,9 +1,11 @@
 #ifndef __TYPE_H__
 #define __TYPE_H__
 
+#include <stdexcept>
 #include <string>
 
 using std::string;
+using std::runtime_error;
 
 /* I might change this to something more flexible in the future */
 enum Type
@@ -17,5 +19,13 @@ enum Type
 };
 
 string type_to_str(Type t);
+
+class InvalidTypeError : public runtime_error
+{
+	public:
+		InvalidTypeError(Type g, Type e)
+			: runtime_error("Got " + type_to_str(g) +
+					", expected " + type_to_str(e)) {}
+};
 
 #endif
