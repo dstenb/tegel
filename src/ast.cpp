9781fed018e367f5dac64fff4ddd4cc94cc3cd90
dstenb <dstenb@gmail.com>
2013-07-01 14:46:27 +0200

    Implemented accept functions

---
+++ src/ast.cpp
@@ -0,0 +1,24 @@
+#include "ast.hpp"
+
+namespace ast {
+
+/* Accept function macro, quite ugly, quite smooth */
+#define GENERATE_ACCEPT(x) void x::accept(AST_Visitor &v) { v.visit(this); }
+
+GENERATE_ACCEPT(And)
+GENERATE_ACCEPT(Or)
+GENERATE_ACCEPT(Plus)
+GENERATE_ACCEPT(Minus)
+GENERATE_ACCEPT(Times)
+GENERATE_ACCEPT(Constant)
+GENERATE_ACCEPT(FunctionCall)
+GENERATE_ACCEPT(SymbolRef)
+
+GENERATE_ACCEPT(For)
+GENERATE_ACCEPT(If)
+GENERATE_ACCEPT(Elif)
+GENERATE_ACCEPT(Else)
+GENERATE_ACCEPT(Text)
+GENERATE_ACCEPT(InlinedExpression)
+
+}
