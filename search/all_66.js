9695940f08d20995675684ee6eb448fc703fc120
dstenb <dstenb@gmail.com>
2013-07-02 10:41:12 +0200

    Updated the documentation

--- search/all_66.js
+++ search/all_66.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['for',['For',['../classast_1_1For.html',1,'ast']]],
-  ['functioncall',['FunctionCall',['../classast_1_1FunctionCall.html',1,'ast']]]
+  ['functioncall',['FunctionCall',['../classast_1_1FunctionCall.html',1,'ast']]],
+  ['functions',['Functions',['../tegel_functions.html',1,'']]]
 ];
