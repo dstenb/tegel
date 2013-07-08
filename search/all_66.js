41f71a05b711ea2b1184f1d30ae8536f1ca0a241
dstenb <dstenb@gmail.com>
2013-07-08 14:01:58 +0200

    Updated the documentation

--- search/all_66.js
+++ search/all_66.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['foreach',['ForEach',['../classast_1_1ForEach.html',1,'ast']]],
-  ['functionprototyp',['FunctionPrototyp',['../classFunctionPrototyp.html',1,'']]],
-  ['functionprototype',['FunctionPrototype',['../classFunctionPrototype.html',1,'']]]
+  ['foreach',['ForEach',['../classast_1_1ForEach.html',1,'ast']]]
 ];
