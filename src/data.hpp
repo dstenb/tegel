da4f9085525c81f6e5d6d673fd2edfcf54946e89
dstenb <dstenb@gmail.com>
2013-07-23 13:11:18 +0200

    Added ParseData struct

---
+++ src/data.hpp
@@ -0,0 +1,24 @@
+#ifndef __PARSE_DATA_H__
+#define __PARSE_DATA_H__
+
+#include "symbol.hpp"
+#include "type.hpp"
+
+struct ParseData
+{
+        ParseData()
+            : root_table(new symbol::SymbolTable),
+              current_table(root_table),
+              arguments(), body(nullptr) {}
+
+        symbol::SymbolTable *root_table;
+        symbol::SymbolTable *current_table;
+        vector<symbol::Argument *> arguments;
+        ast::Statements *body;
+
+    private:
+        ParseData(const ParseData &) = delete;
+        ParseData &operator=(const ParseData &) = delete;
+};
+
+#endif
